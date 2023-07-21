cbcdd17de919a9cc2fb93e5a7f596e99d25adce3
Roger Canayon Jr <rcanayonjr@gmail.com>
2023-07-22 00:41:54 +0800

    Start Special Machine

---
+++ SpecialMachine.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class SpecialMachine extends RegularMachine {
+    public SpecialMachine(ArrayList<Items> items, int[][] machineBalance)
+    {
+        super(items, machineBalance);
+    }
+}
