46e5788e06969b792597a2b9109c7fd9bbda812b
Roger Canayon Jr <rcanayonjr@gmail.com>
2023-07-24 14:34:49 +0800

    Add Main Menu GUI

--- MCO2_PROG3/src/Factory.java
+++ MCO2_PROG3/src/Factory.java
@@ -25,20 +25,64 @@ public class Factory extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        mainMenuPanel = new javax.swing.JPanel();
+        VendingMachineLabel = new javax.swing.JLabel();
+        FactoryLabel = new javax.swing.JLabel();
+        CreateBtn = new javax.swing.JButton();
+        TestBtn = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        BackgroundPicture = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(665, 590));
         setResizable(false);
         setSize(new java.awt.Dimension(665, 590));
 
+        mainMenuPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        VendingMachineLabel.setBackground(null);
+        VendingMachineLabel.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        VendingMachineLabel.setForeground(new java.awt.Color(255, 255, 255));
+        VendingMachineLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        VendingMachineLabel.setText("Vending Machine");
+        mainMenuPanel.add(VendingMachineLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 170, -1, -1));
+
+        FactoryLabel.setBackground(null);
+        FactoryLabel.setFont(new java.awt.Font("Arial", 1, 48)); // NOI18N
+        FactoryLabel.setForeground(new java.awt.Color(255, 255, 255));
+        FactoryLabel.setText("FACTORY");
+        mainMenuPanel.add(FactoryLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 200, 249, -1));
+
+        CreateBtn.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        CreateBtn.setText("Create");
+        CreateBtn.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        mainMenuPanel.add(CreateBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 270, 230, 46));
+
+        TestBtn.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        TestBtn.setText("Test");
+        TestBtn.setToolTipText("");
+        TestBtn.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        mainMenuPanel.add(TestBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 340, 230, 46));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel1.setText("by Roger and Kristian");
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        mainMenuPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 420, 160, -1));
+
+        BackgroundPicture.setIcon(new javax.swing.ImageIcon("/Users/rogercanayon/Documents/college/dlsu/term3/CCPROG3/MCO1_CCPROG3/MCO2_PROG3/Elements/backgroundMainMenu.png")); // NOI18N
+        mainMenuPanel.add(BackgroundPicture, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 670, 590));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(mainMenuPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(mainMenuPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -80,5 +124,12 @@ public class Factory extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BackgroundPicture;
+    private javax.swing.JButton CreateBtn;
+    private javax.swing.JLabel FactoryLabel;
+    private javax.swing.JButton TestBtn;
+    private javax.swing.JLabel VendingMachineLabel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel mainMenuPanel;
     // End of variables declaration//GEN-END:variables
 }
