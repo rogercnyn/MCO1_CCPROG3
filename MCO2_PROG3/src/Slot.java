20e537c76f8f4559bcb2389aea59fc6a46007b45
Kristian Kintanar <kristiankintanar11@gmail.com>
2023-07-31 00:50:41 +0800

    Fixed Slot

--- MCO2_PROG3/src/Slot.java
+++ MCO2_PROG3/src/Slot.java
@@ -53,48 +53,93 @@ public class Slot {
         Sinker nata = new Sinker("Nata de Coco", 100);
 
         Milktea okinawapearls = new Milktea("Okinawa with Pearls", okinawa, pearls, 135);
-        Stack<Item> itemOne = addItem(okinawapearls);
+        Item itemone = new Item("okinawapearls", 118);
+        Stack<Item> itemOne = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemOne.push(itemone);
+        }
         Slot slot1 = new Slot(itemOne);
         regularItems.add(slot1);
 
         Milktea okinawawhitepearls = new Milktea("Okinawa with White Pearls", okinawa, whitepearls, 135);
-        Stack<Item> itemTwo = addItem(okinawawhitepearls);
+        Item itemtwo = new Item("okinawhitepearls", 120);
+        Stack<Item> itemTwo = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemTwo.push(itemtwo);
+        }
         Slot slot2 = new Slot(itemTwo);
         regularItems.add(slot2);
 
         Milktea okinawanata = new Milktea("Okinawa with Nata", okinawa, nata, 135);
-        Stack<Item> itemThree = addItem(okinawanata);
+        Item itemthree = new Item("okinawanta", 130);
+        Stack<Item> itemThree = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemThree.push(itemthree);
+        }
         Slot slot3 = new Slot(itemThree);
         regularItems.add(slot3);
 
         Milktea wintermelonpearls = new Milktea("Wintermelon with Pearls", wintermelon, pearls, 135);
-        Stack<Item> itemFour = addItem(wintermelonpearls);
+        Item itemfour = new Item("wintermelonpearls", 118);
+        Stack<Item> itemFour = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemFour.push(itemfour);
+        }
         Slot slot4 = new Slot(itemFour);
         regularItems.add(slot4);
 
         Milktea wintermelonwhitepearls = new Milktea("Wintermelon with White Pearls", wintermelon, whitepearls, 135);
-        Stack<Item> itemFive = addItem(wintermelonwhitepearls);
+        Item itemfive = new Item("wintermelonwhitepearls", 120);
+        Stack<Item> itemFive = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemFive.push(itemfive);
+        }
         Slot slot5 = new Slot(itemFive);
         regularItems.add(slot5);
 
         Milktea wintermelonnata = new Milktea("Wintermelon with Nata", wintermelon, nata, 135);
-        Stack<Item> itemSix = addItem(wintermelonnata);
+         Item itemsix = new Item("wintermelonnata", 130);
+        Stack<Item> itemSix = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemSix.push(itemsix);
+        }
         Slot slot6 = new Slot(itemSix);
         regularItems.add(slot6);
 
         Milktea hokkaidopearls = new Milktea("Hokkaido with Pearls", hokkaido, pearls, 135);
-        Stack<Item> itemSeven = addItem(hokkaidopearls);
+         Item itemseven = new Item("hokkaidopearls", 120);
+        Stack<Item> itemSeven = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemSeven.push(itemseven);
+        }
         Slot slot7 = new Slot(itemSeven);
         regularItems.add(slot7);
 
         Milktea hokkaidowhitepearls = new Milktea("Hokkaido with White Pearls", hokkaido, whitepearls, 135);
-        Stack<Item> itemEight = addItem(hokkaidowhitepearls);
+        Item itemeight = new Item("hokkaidowhitepearls", 118);
+        Stack<Item> itemEight = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemEight.push(itemeight);
+        }
         Slot slot8 = new Slot(itemEight);
         regularItems.add(slot8);
 
         Milktea hokkaidonata = new Milktea("Hokkaido with Nata", hokkaido, nata, 135);
-        Stack<Item> itemNine = addItem(hokkaidonata);
-        Slot slot9 = new Slot(itemNine);
+        Item itemnine = new Item("hokkaidonata", 130);
+        Stack<Item> itemNinesStack = new Stack();
+        for(int i=0;i<10;i++)
+        {
+            itemNinesStack.push(itemnine);
+        }
+        Slot slot9 = new Slot(itemNinesStack);
         regularItems.add(slot9);
 
         return regularItems;
